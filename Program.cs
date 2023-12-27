78db1fa20fad37b0d3c147b8da38bdacec29b2e5
JaquelineMelo <jaquelinewebnet@gmail.com>
2023-12-27 16:18:31 -0300

    Implementação do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "Modelo Smart", imei: "41558755", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4521", modelo: "Modelo Premium", imei: "415587155", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
